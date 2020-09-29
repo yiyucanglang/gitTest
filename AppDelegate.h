b6a00f51ff96f77959b867318cfae312f90a405d
zhouyijin <zhouyijin>
2020-09-29 18:58:36 +0800

    提交说明

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EasyListViewDemo
+//
+//  Created by FDadmin on 2020/7/30.
+//  Copyright © 2020 FDadmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property(nonatomic, strong) UIWindow *window;
+@end
+
